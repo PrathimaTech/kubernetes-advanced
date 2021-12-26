8f4b6e0d31fa44fa86aed746e79ce9a41e78922f
PrathimaTech <91586637+PrathimaTech@users.noreply.github.com>
2021-12-26 10:50:52 +0530

    Update setup-user.sh

--- install/setup-user.sh
+++ install/setup-user.sh
@@ -107,7 +107,7 @@ update_conf()
 
 USER="devops"
 GROUP="devops"
-passw="devops@123"
+passw="devops"
 
 if [ -f /etc/os-release ];then
    osname=`grep ID /etc/os-release | egrep -v 'VERSION|LIKE|VARIANT' | cut -d'=' -f2 | sed -e 's/"//' -e 's/"//'`
